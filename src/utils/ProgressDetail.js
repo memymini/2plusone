d0bdf5b67dc73617309725b5e23f9960d5537216
Kyumin Lee <kmkm0903@naver.com>
2023-07-29 12:36:53 +0900

    feat: 진행중메인페이지 진행중상세페이지

---
+++ src/utils/ProgressDetail.js
@@ -0,0 +1,95 @@
+import Progress0 from "../assets/ProgressImage/Progress0.jpg";
+import Progress1 from "../assets/ProgressImage/Progress1.jpg";
+import Progress2 from "../assets/ProgressImage/Progress2.jpg";
+import Progress3 from "../assets/ProgressImage/Progress3.jpg";
+import Progress4 from "../assets/ProgressImage/Progress4.jpg";
+import Progress5 from "../assets/ProgressImage/Progress5.jpg";
+
+export const ProgressImage = [
+    {id: "0", name: "Progress0", src: Progress0},
+ 
+
+];
+export const contents = [
+         {
+           title: "폭우로 인해 집과 가족을 잃은 이들",
+           organization: "사회지원재단",
+           image: Progress0,
+           progress: 21,
+           barWidth: "290px",
+           totalTokens: "209,000",
+           targetTokens: "1,200,000",
+           categories: ["all", "disaster"],
+           date: "2023년",
+        
+         },
+         {
+           title: "배고픔에 익숙해진 규민이를 도와주세요.",
+           organization: "서울시희망조합",
+           image: Progress1,
+           progress: 32,
+           barWidth: "280px",
+           totalTokens: "480,000",
+           targetTokens: "12,300,000",
+           categories: ["all", "weak"],
+           date: "2023년",
+         },
+         {
+           title: "어르신들에게 선풍기를 지원해주세요.",
+           organization: "사회지원재단",
+           image: Progress2,
+           progress: 75,
+           barWidth: "311px",
+           totalTokens: "10,947,000",
+           targetTokens: "12,300,000",
+           categories: ["all", "weak"],
+           date: "2023년",
+           text: `저희 지역은 뜨거운 여름철이 찾아왔습니다. 특히 우리 어르신들은 더위로 인해 힘들어하고 계실 것입니다. 
+           그동안 우리 지역 사랑으로 많은 도움을 주신 어르신들께 보답하는 시간이 왔습니다.
+           '사랑의 선풍기 기부 캠페인'을 시작합니다. 
+           이번 캠페인은 우리 지역 어르신들에게 선풍기를 지원하여 더위를 덜어주는 작은 도움을 드리고자 합니다.
+           여러분의 따뜻한 마음과 기부로 우리 어르신들의 여름을 쾌적하게 만들어주세요.`
+         },
+         {
+           title: "지영이, 혼자가 아니야!",
+           organization: "굿네이버스",
+           image: Progress3,
+           progress: 42,
+           barWidth: "147px",
+           totalTokens: "5,166,000",
+           targetTokens: "12,300,000",
+           categories: ["all", "weak"],
+           date: "2023년",
+           text: `
+           제대로 된 식사를 하지 못하고 있는 규민이
+           
+           규민이는 아무런 잘못이 없음에도 불구하고, 가난과 궁핍함으로 인해 매일매일 배고픔과 싸워야 합니다. 
+           아침이면 한 끼의 식사를 위해 눈을 뜨지만, 라면이나 인스턴트 식품으로 배를 채울 수 밖에 없습니다. 
+           그럼에도 불구하고 규민이는 포기하지 않고 용기와 희망을 가지고 살아가고 있습니다. 
+           이번 방학에도 급식 사각지대 없이 모든 아동이 따뜻한 보살핌 속에서 방학을 보낼 수 있게, 눈치 보면서 밥 먹는 아이들이 없도록 도시락을 선물해주세요! 
+           `
+         },
+         {
+           title: "혼자 아이를 키우는 엄마들을 응원해주세요.",
+           organization: "한국재난관리협회",
+           image: Progress4,
+           progress: 35,
+           barWidth: "122px",
+           totalTokens: "4,350,000",
+           targetTokens: "12,300,000",
+           categories: ["all", "weak"],
+           date: "2023년",
+         },
+         {
+           title: "코코가 다시 뛰어놀 수 있게 도와주세요.",
+           organization: "서울동물권리연합",
+           image: Progress5,
+           progress: 21,
+           barWidth: "73px",
+           totalTokens: "2,583,000",
+           targetTokens: "12,300,000",
+           categories: ["all", "animal"],
+           date: "2023년",
+         },
+         // 다른 컨텐츠들도 추가...
+       ];
